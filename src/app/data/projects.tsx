25ec825e9d107391b73fc74643aad6f74c5cd6dd
rchouhani <rchouhani@me.com>
2025-09-30 16:47:34 +0200

    UPDATE : Add deteils project

--- src/app/data/projects.tsx
+++ src/app/data/projects.tsx
@@ -6,11 +6,13 @@ export const projects: ProjectCardProps[] = [
   //   image: "/pten.jpg",
   //   githubUrl: "https://github.com/rchouhani/ptenofficiel",
   //   deployUrl: "",
+  //   detailsProject: "dsfsg",  
   // },
   {
     name: "ZeDeN",
     image: "/zeden.jpg",
     deployUrl: "https://www.zeden.net/",
+    detailsProject: "Je travaille bénévolement chez ZeDeN en tant que développeur Web. Je gère principalement des petits bugs pour le moment via l'interface de ticket du site.",
   },
   {
     name: "Click & Cook",
@@ -18,6 +20,7 @@ export const projects: ProjectCardProps[] = [
     githubFrontUrl: "https://github.com/Edwige08/click_and_cook_front",
     githubBackUrl: "https://github.com/rchouhani/click_and_cook_back",
     deployUrl: "https://click-and-cook.vercel.app/",
+    detailsProject: "Click & Cook est un projet d'étude que j'ai effectué avec Edwige Saves et Victor Lepron au sein d'Ada Tech School. Nous avons décidés de travailler back-end sur Django et le Front avec Next JS",
   },
   {
     name: "Adaopte",
@@ -25,17 +28,20 @@ export const projects: ProjectCardProps[] = [
     githubBackUrl: "https://github.com/rchouhani/adaopte_back_tsx",
     githubFrontUrl: "https://github.com/fatydm/adaopte_front-tsx",
     deployUrl: "https://adaopte-rofated.vercel.app/",
+    detailsProject: "dsfsg",
   },
   {
     name: "Surfin' Safari",
     image: "/surfinsafari.jpg",
     githubUrl: "https://github.com/adatechschool/projet-front-end-conception-de-base-de-donn-es-rchouhani", // deploiement après changement de thème ( vélo )
     deployUrl: "",
+    detailsProject: "dsfsg",
   },
   // {
   //   name: "Global Fooder",
   //   image: "/globalfooder.png",
   //   githubUrl:"https://github.com/rchouhani/dataviz_project", // deploiement possible
   //   deployUrl: "",
+  //   detailsProject: "dsfsg",
   // },
 ];
