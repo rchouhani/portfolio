6eb9363de16b520b9e5c66921acda95098d8812a
rchouhani <rchouhani@me.com>
2025-10-02 14:04:12 +0200

    UPDATE : Link instead of <a> to deploy on vercel

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { AtSign, Github, Home, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="flex flex-row justify-center items-center space-x-70">
-      <a href="/" aria-label="Revenir à la page d'accueil"><Home stroke="black" strokeWidth={2} height={50} width={50} /></a>
-      <a href="https://www.linkedin.com/in/romain-chouhani-334b1586/" aria-label="Page Linked In de Romain Chouhani"><Linkedin stroke="black" strokeWidth={2} height={50} width={50} /></a>
-      <a href="https://github.com/rchouhani" aria-label="Profil Github de Romain Chouhani"><Github stroke="black" strokeWidth={2} height={50} width={50} /></a>
-      <a href="mailto:rchouhani@me.com" aria-label="Envoyer un mail à Romain Chouhani"><AtSign stroke="black" strokeWidth={2} height={50} width={50} /></a>
+      <Link href="/" aria-label="Revenir à la page d'accueil"><Home stroke="black" strokeWidth={2} height={50} width={50} /></Link>
+      <Link href="https://www.linkedin.com/in/romain-chouhani-334b1586/" aria-label="Page Linked In de Romain Chouhani"><Linkedin stroke="black" strokeWidth={2} height={50} width={50} /></Link>
+      <Link href="https://github.com/rchouhani" aria-label="Profil Github de Romain Chouhani"><Github stroke="black" strokeWidth={2} height={50} width={50} /></Link>
+      <Link href="mailto:rchouhani@me.com" aria-label="Envoyer un mail à Romain Chouhani"><AtSign stroke="black" strokeWidth={2} height={50} width={50} /></Link>
     </nav>
   );
 }
