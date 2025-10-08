7d606e057e397321d65968f9d57057bf5dd64d7e
rchouhani <rchouhani@me.com>
2025-10-08 18:22:26 +0200

    DELETE : navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,13 @@
 import Carroussel from "../components/Carroussel";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import { projects } from "../data/projects";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="flex flex-col items-center">
-        <h1 className="text-3xl mt-15 -mb-15 text-center">Mes Projets</h1>
+        <h1 className="text-[#FFFF00] text-3xl mt-15 -mb-15 text-center colo">Mes Projets</h1>
         <Carroussel projects={projects} />
       </main>
       <Footer />
