d8864185a621cfdc78b4b1b727c6095c58e4856c
rchouhani <rchouhani@me.com>
2026-06-26 19:46:15 +0200

    delete/ carrousel

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,18 @@
-import Carroussel from "../components/Carroussel";
+import ProjectCard from "../components/ProjectCard";
 import { projects } from "../data/projects";
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
       <main className="flex flex-col items-center w-full">
         <h1 className="text-[#FFFF00] text-3xl sm:text-4xl md:text-5xl mt-32 text-center">
           Mes Projets
         </h1>
-        <Carroussel projects={projects} />
+        <div className="grid grid-cols-2 gap-10 mt-10 px-6 w-full max-w-4xl mx-auto">
+          {projects.map((project, i) => (
+            <ProjectCard key={i} {...project} />
+          ))}
+        </div>
       </main>
     </>
   );
